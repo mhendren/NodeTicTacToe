7ae2844da0a3f6d57d6a5760f82970950ba46e03
Michael Hendren <michael.hendren@gmail.com>
2014-11-09 12:36:50 -0600

    Implemented tests for the evaluator.

--- test/AI/Evaluator-test.js
+++ test/AI/Evaluator-test.js
@@ -15,7 +15,7 @@ describe("invalid board", function() {
     it("should not have an invalid board on an empty board with 9 squares", function(done) {
         expect(evaluator.isValid.bind(evaluator, "---------")).to.not.throw("Board size is not exactly 9 squares");
         done();
-    })
+    });
     it("should have an invalid board if there is an invalid mark (not X, O, or empty) [---n-----]", function(done) {
         expect(evaluator.isValid.bind(evaluator, "---n-----")).to.throw("Invalid square in board");
         done();
@@ -35,71 +35,263 @@ describe("invalid board", function() {
 });
 
 describe("winner with rows all Xs", function() {
-    it('should find winner X [XXX------] with top row all Xs');
-    it("should find winner X [---XXX---] with middle row all Xs");
-    it("should find winner X [------XXX] with bottom row all Xs");
+    it('should find winner X [XXX------] with top row all Xs', function(done) {
+        var winners = evaluator.winner("XXX------");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('X');
+        expect(winners[0].winAt).to.deep.equal([0, 1, 2]);
+        done();
+    });
+    it("should find winner X [---XXX---] with middle row all Xs", function(done) {
+        var winners = evaluator.winner("---XXX---");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('X');
+        expect(winners[0].winAt).to.deep.equal([3, 4, 5]);
+        done();
+    });
+    it("should find winner X [------XXX] with bottom row all Xs", function(done) {
+        var winners = evaluator.winner("------XXX");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('X');
+        expect(winners[0].winAt).to.deep.equal([6, 7, 8]);
+        done();
+    });
 });
 
 describe("winner with rows all Os", function() {
-    it("should find winner O [OOO------] with top row all Os");
-    it("should find winner O [---OOO---] with middle row all Os");
-    it("should find winner O [------OOO] with bottom row all Os");
+    it("should find winner O [OOO------] with top row all Os", function(done) {
+        var winners = evaluator.winner("OOO------");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('O');
+        expect(winners[0].winAt).to.deep.equal([0, 1, 2]);
+        done();
+    });
+    it("should find winner O [---OOO---] with middle row all Os", function(done) {
+        var winners = evaluator.winner("---OOO---");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('O');
+        expect(winners[0].winAt).to.deep.equal([3, 4, 5]);
+        done();
+    });
+    it("should find winner O [------OOO] with bottom row all Os", function(done) {
+        var winners = evaluator.winner("------OOO");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('O');
+        expect(winners[0].winAt).to.deep.equal([6, 7, 8]);
+        done();
+    });
 });
 
 describe("winner with all columns Xs", function() {
-    it("should find winner X [X--X--X--] with left column all Xs");
-    it("should find winner X [-X--X--X-] with middle column all Xs");
-    it("should find winner X [--X--X--X] with right column all Xs");
+    it("should find winner X [X--X--X--] with left column all Xs", function(done) {
+        var winners = evaluator.winner("X--X--X--");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('X');
+        expect(winners[0].winAt).to.deep.equal([0, 3, 6]);
+        done();
+    });
+    it("should find winner X [-X--X--X-] with middle column all Xs", function(done) {
+        var winners = evaluator.winner("-X--X--X-");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('X');
+        expect(winners[0].winAt).to.deep.equal([1, 4, 7]);
+        done();
+    });
+    it("should find winner X [--X--X--X] with right column all Xs", function(done) {
+        var winners = evaluator.winner("--X--X--X");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('X');
+        expect(winners[0].winAt).to.deep.equal([2, 5, 8]);
+        done();
+    });
 });
 
 describe("winner with all columns Os", function() {
-    it("should find winner O [O--O--O--] with left column all Os");
-    it("should find winner O [-O--O--O-] with middle column all Os");
-    it("should find winner O [--O--O--O] with right column all Os")
+    it("should find winner O [O--O--O--] with left column all Os", function(done) {
+        var winners = evaluator.winner("O--O--O--");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('O');
+        expect(winners[0].winAt).to.deep.equal([0, 3, 6]);
+        done();
+    });
+    it("should find winner O [-O--O--O-] with middle column all Os", function(done) {
+        var winners = evaluator.winner("-O--O--O-");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('O');
+        expect(winners[0].winAt).to.deep.equal([1, 4, 7]);
+        done();
+    });
+    it("should find winner O [--O--O--O] with right column all Os", function(done) {
+        var winners = evaluator.winner("--O--O--O");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('O');
+        expect(winners[0].winAt).to.deep.equal([2, 5, 8]);
+        done();
+    })
 });
 
 describe("winner with diagonal Xs", function() {
-    it("should find winner X [X---X---X] with top left to bottom right diagonal all Xs");
-    it("should find winner X [--X-X-X--] with top right to bottom left diagonal all Xs");
+    it("should find winner X [X---X---X] with top left to bottom right diagonal all Xs", function(done) {
+        var winners = evaluator.winner("X---X---X");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('X');
+        expect(winners[0].winAt).to.deep.equal([0, 4, 8]);
+        done();
+    });
+    it("should find winner X [--X-X-X--] with top right to bottom left diagonal all Xs", function(done) {
+        var winners = evaluator.winner("--X-X-X--");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('X');
+        expect(winners[0].winAt).to.deep.equal([2, 4, 6]);
+        done();
+    });
 });
 
 describe("winner with diagonal Os", function() {
-    it("should find winner O [O---O---O] with top left to bottom right diagonal all Os");
-    it("should find winner O [--O-O-O--] with top right to bottom left diagonal all Os");
+    it("should find winner O [O---O---O] with top left to bottom right diagonal all Os", function(done) {
+        var winners = evaluator.winner("O---O---O");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('O');
+        expect(winners[0].winAt).to.deep.equal([0, 4, 8]);
+        done();
+    });
+    it("should find winner O [--O-O-O--] with top right to bottom left diagonal all Os", function(done) {
+        var winners = evaluator.winner("--O-O-O--");
+        expect(winners.length).to.equal(1);
+        expect(winners[0].player).to.equal('O');
+        expect(winners[0].winAt).to.deep.equal([2, 4, 6]);
+        done();
+    });
 });
 
 describe("no winner incomplete board", function() {
-    it("should not find a winner on empty board [---------]");
-    it("should not find a winner on incomplete board1 [XO-------]");
-    it("should not find a winner on incomplete board2 [XOX-XO---]");
-    it("should not find a winner on incomplete board3 [OOXX-XOXO]");
-    it("should not find a winner on incomplete board4 [XOXOXO-XO]");
+    it("should not find a winner on empty board [---------]", function(done) {
+        var winners = evaluator.winner("---------");
+        expect(winners.length).to.equal(0);
+        done();
+    });
+    it("should not find a winner on incomplete board1 [XO-------]", function(done) {
+        var winners = evaluator.winner("XO-------");
+        expect(winners.length).to.equal(0);
+        done();
+    });
+    it("should not find a winner on incomplete board2 [XOX-XO---]", function(done) {
+        var winners = evaluator.winner("XOX-XO---");
+        expect(winners.length).to.equal(0);
+        done();
+    });
+    it("should not find a winner on incomplete board3 [OOXX-XOXO]", function(done) {
+        var winners = evaluator.winner("OOXX-XOXO");
+        expect(winners.length).to.equal(0);
+        done();
+    });
+    it("should not find a winner on incomplete board4 [XOXOXO-XO]", function(done) {
+        var winners = evaluator.winner("XOXOXO-XO");
+        expect(winners.length).to.equal(0);
+        done();
+    });
 });
 
 describe("identify a complete board", function() {
-    it("should not find a complete board on incomplete board1 [XO-------]");
-    it("should not find a complete board on incomplete board2 [XOX-XO---]");
-    it("should not find a complete board on incomplete board3 [OOXX-XOXO]");
-    it("should not find a complete board on incomplete board4 [OOXX-XOXO]");
-    it("should find a complete board on complete board1 [XOXXOOOXX]");
-    it("should find a complete board on complete board2 [OXOXXOXOX]");
-    it("should find a complete board on complete board3 [XXOOOXXOX]" );
-    it("should find a complete board on complete board4 [OXXXOXOOX]");
+    it("should not find a complete board on incomplete board1 [XO-------]", function(done) {
+        var complete = evaluator.isComplete("XO-------");
+        expect(complete).to.be.false;
+        done();
+    });
+    it("should not find a complete board on incomplete board2 [XOX-XO---]", function(done) {
+        var complete = evaluator.isComplete("XOX-XO---");
+        expect(complete).to.be.false;
+        done();
+    });
+    it("should not find a complete board on incomplete board3 [OOXX-XOXO]", function(done) {
+        var complete = evaluator.isComplete("OOXX-XOXO");
+        expect(complete).to.be.false;
+        done();
+    });
+    it("should not find a complete board on incomplete board4 [XOXOXO-XO]", function(done) {
+        var complete = evaluator.isComplete("XOXOXO-XO");
+        expect(complete).to.be.false;
+        done();
+    });
+    it("should find a complete board on complete board1 [XOXXOOOXX]", function(done) {
+        var complete = evaluator.isComplete("XOXXOOOXX");
+        expect(complete).to.be.true;
+        done();
+    });
+    it("should find a complete board on complete board2 [OXOXXOXOX]", function(done) {
+        var complete = evaluator.isComplete("OXOXXOXOX");
+        expect(complete).to.be.true;
+        done();
+    });
+    it("should find a complete board on complete board3 [XXOOOXXOX]", function(done){
+        var complete = evaluator.isComplete("XXOOOXXOX");
+        expect(complete).to.be.true;
+        done();
+    });
+    it("should find a complete board on complete board4 [OXXXOXOOX]", function(done) {
+        var complete = evaluator.isComplete("OXXXOXOOX");
+        expect(complete).to.be.true;
+        done();
+    });
 });
 
 describe("no winner complete board", function() {
-    it("should not find a winner on tie board1 [XOXXOOOXX]");
-    it("should not find a winner on tie board2 [OXOXXOXOX]");
-    it("should not find a winner on tie board3 [XXOOOXXOX]");
-    it("should not find a winner on tie board4 [OXXXOOOXX]");
+    it("should not find a winner on tie board1 [XOXXOOOXX]", function(done) {
+        var winner = evaluator.winner("XOXXOOOXX");
+        expect(winner.length).to.equal(0);
+        done();
+    });
+    it("should not find a winner on tie board2 [OXOXXOXOX]", function(done) {
+        var winner = evaluator.winner("OXOXXOXOX");
+        expect(winner.length).to.equal(0);
+        done();
+    });
+    it("should not find a winner on tie board3 [XXOOOXXOX]", function(done) {
+        var winner = evaluator.winner("XXOOOXXOX");
+        expect(winner.length).to.equal(0);
+        done();
+    });
+    it("should not find a winner on tie board4 [OXXXOOOXX]", function(done) {
+        var winner = evaluator.winner("OXXXOOOXX");
+        expect(winner.length).to.equal(0);
+        done();
+    });
 });
 
 describe("find a tie on a tie board", function() {
-    it("should not find a tie on incomplete board1 [XOXXOOOXX]");
-    it("should not find a tie on incomplete board2 (with winner) [OXXXOXOOX]");
-    it("should not find a tie on complete board with winner X [XOXOOXOXX]]");
-    it("should find a tie on tie board1 [XOXXOOOXX]");
-    it("should find a tie on tie board2 [OXOXOXXOX]");
-    it("should find a tie on tie board3 [XXOOOXXOX]");
-    it("should find a tie on tie board4 [OXXXOOOXX]");
+    it("should not find a tie on complete board1 [XOXOXOOXX]", function(done) {
+        var tie = evaluator.isTie("XOXOXOOXX");
+        expect(tie).to.be.false;
+        done();
+    });
+    it("should not find a tie on complete board2 [OXXXOXOOX]", function(done) {
+        var tie = evaluator.isTie("OXXXOXOOX");
+        expect(tie).to.be.false;
+        done();
+    });
+    it("should not find a tie on complete board3 [XOXOOXOXX]]", function(done) {
+        var tie = evaluator.isTie("XOXOOXOXX");
+        expect(tie).to.be.false;
+        done();
+    });
+    it("should find a tie on tie board1 [XOXXOOOXX]", function(done) {
+        var tie = evaluator.isTie("XOXXOOOXX");
+        expect(tie).to.be.true;
+        done();
+    });
+    it("should find a tie on tie board2 [OXOXOXXOX]", function(done) {
+        var tie = evaluator.isTie("OXOXOXXOX");
+        expect(tie).to.be.true;
+        done();
+    });
+    it("should find a tie on tie board3 [XXOOOXXOX]", function(done) {
+        var tie = evaluator.isTie("XXOOOXXOX");
+        expect(tie).to.be.true;
+        done();
+    });
+    it("should find a tie on tie board4 [OXXXOOOXX]", function(done) {
+        var tie = evaluator.isTie("OXXXOOOXX");
+        expect(tie).to.be.true;
+        done();
+    });
 });
