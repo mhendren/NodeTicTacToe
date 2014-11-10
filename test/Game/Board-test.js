484e6446344f4ec86644f998d1d5ff7a7116de8f
Michael Hendren <michael.hendren@gmail.com>
2014-11-09 22:21:08 -0600

    Added additional tests for error conditions and the getSquare() method for the board.

--- test/Game/Board-test.js
+++ test/Game/Board-test.js
@@ -4,10 +4,31 @@ var expect = chai.expect;
 
 describe("New Board", function() {
     it("should return a board that has all dashes");
+    it("should create a board based on the passed in layout");
+    it("should throw an error if the passed in layout is invalid");
+    it("should throw an error if the passed in board is an invalid type");
 });
 
 describe("Player Move", function() {
     it("should have a board with an X in the designated location (0,0)");
     it("should add an O at the designated location (1, 1)");
     it("should not allow an X to be placed at the designated location (1, 1)");
+    it("should throw an error if there is no row specified");
+    it("should throw an error if there is no column specified");
+    it("should throw an error if the row is out of range > 2");
+    it("should throw an error if the column is out of range > 2");
+    it("should throw an error if the row is out of range < 0");
+    it("should throw an error if the column is out of range < 0");
+    it("should throw an error if the row is an invalid type");
+    it("should throw an error if the column is an invalid type");
+});
+
+describe("Get square", function() {
+    it("should return the appropriate value for all squares in layout [XO-XXOX-O]");
+    it("should throw an error if the row is out of range > 2");
+    it("should throw an error if the column is out of range > 2");
+    it("should throw an error if the row is out of range < 0");
+    it("should throw an error if the column is out of range < 0");
+    it("should throw an error if the row is an invalid type");
+    it("should throw an error if the column is an invalid type");
 });
