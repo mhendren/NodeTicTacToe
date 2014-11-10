08e86ff6bf1f72dd6c9e8d8dfd9a76db58dca253
Michael Hendren <michael.hendren@gmail.com>
2014-11-09 20:34:03 -0600

    Added tests for the Board component

---
+++ test/Game/Board.js
@@ -0,0 +1,13 @@
+
+var chai = require("chai");
+var expect = chai.expect;
+
+describe("New Board", function() {
+    it("should return a board that has all dashes");
+});
+
+describe("Player Move", function() {
+    it("should have a board with an X in the designated location (0,0)");
+    it("should add an O at the designated location (1, 1)");
+    it("should not allow an X to be placed at the designated location (1, 1)");
+});
