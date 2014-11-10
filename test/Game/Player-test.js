5c718774887bc731dbb095f61fec02ad511b7288
Michael Hendren <michael.hendren@gmail.com>
2014-11-09 21:21:27 -0600

    Added tests for the Player component Updated Board to Board-test

---
+++ test/Game/Player-test.js
@@ -0,0 +1,23 @@
+
+var chai = require("chai");
+var expect = chai.expect;
+
+describe("Create player", function() {
+    it("should create an unset player with no type");
+    it("should create an 'X' player, with no type");
+    it("should create an 'O' player, with no type");
+    it("should create an 'X' player type 'human'");
+    it("should create an 'O' player type 'human'");
+    it("should create an 'X' player type 'computer'");
+    it("should create an 'O' player type 'computer'");
+});
+
+describe("Set player", function() {
+    it("should set the player to 'X'");
+    it("should set the player to 'O'");
+});
+
+describe("Set player type", function() {
+    it("should set the player type to 'human'");
+    it("should set the player type to 'computer'");
+});
